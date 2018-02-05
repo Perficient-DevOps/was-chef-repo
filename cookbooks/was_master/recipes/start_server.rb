6ed24da2f02dcd1f5b6c21f2fe4320f1302e5239
Rory Siems <roryrs@gmail.com>
2018-02-05 10:24:43 -0800

    updating attributes

--- cookbooks/was_master/recipes/start_server.rb
+++ cookbooks/was_master/recipes/start_server.rb
@@ -3,6 +3,6 @@
 # Recipe:: default
 #
 # Copyright (c) 2018 The Authors, All Rights Reserved.
-execute "Starting Server #{ node['start_server']['server_name']} " do
+execute "Starting Server #{ node['was']['server_name']} " do
   command "#{ node['was']['profile_path'] }/bin/startServer.sh #{ node['was']['server_name']} -profileName #{node['was']['profile_name']}"
 end
