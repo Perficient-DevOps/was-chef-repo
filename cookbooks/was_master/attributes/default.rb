8e925926d7062f287c728b6afddc445ae4b9a8a2
Rory Siems <roryrs@gmail.com>
2018-02-06 15:18:33 -0800

    Fixing password field.

--- cookbooks/was_master/attributes/default.rb
+++ cookbooks/was_master/attributes/default.rb
@@ -5,7 +5,7 @@ default['was']['cell_name']     = 'STLSCVMG95219Cell01'
 default['was']['node_name']     = 'STLSCVMG95218Node02'
 default['was']['server_name']   = 'rory'
 default['was']['was_user']	= 'wasadmin'
-default['was']['was_pass]']	 = 'adminwas'
+default['was']['was_pass']	 = 'adminwas'
 default['was']['soap_port']     = '10003'
 
 default['was']['profile_path']  = File.join( node['was']['install_home'], 'profiles', node['was']['profile_name'] )
