f17d93dae3fbbdb045e7a2c76454e7c9b78092bf
Rory Siems <roryrs@gmail.com>
2018-02-07 08:48:00 -0800

    adding wsadmin script for start server

--- cookbooks/was_master/recipes/start_server.rb
+++ cookbooks/was_master/recipes/start_server.rb
@@ -4,5 +4,5 @@
 #
 # Copyright (c) 2018 The Authors, All Rights Reserved.
 execute "Starting Server #{ node['was']['server_name']} " do
-  command "#{ node['was']['install_home'] }/bin/startServer.sh #{ node['was']['server_name']} -profileName #{node['was']['profile_name']}"
+  command "#{ node['was']['profile_path'] }/bin/startServer.sh #{ node['was']['server_name']} -profileName #{node['was']['profile_name']}"
 end
