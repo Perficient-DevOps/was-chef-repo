15264b748e84f152ae921048482720e265e2525b
Rory Siems <roryrs@gmail.com>
2018-02-08 10:37:07 -0800

    Updating path to jython scripts as an attribute

--- cookbooks/was_master/recipes/stop_was_server.rb
+++ cookbooks/was_master/recipes/stop_was_server.rb
@@ -5,5 +5,5 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 execute "Run Jython" do
-  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password #{node['was']['was_pass']} -f /WorkingData/jythonScripts/stopAppServer.py \"#{node['was']['node_name']}\" \"#{node['was']['server_name']}\" \"YES\""
+  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password #{node['was']['was_pass']} -f #{node['was']['jython_path']}stopAppServer.py \"#{node['was']['node_name']}\" \"#{node['was']['server_name']}\" \"YES\""
 end
