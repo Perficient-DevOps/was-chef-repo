aa0997bf7d964645960492600db3f4e4ad081aa0
Rory Siems <roryrs@gmail.com>
2018-02-08 08:14:07 -0800

    Edited start and stop cookbooks to use new jython script.

--- cookbooks/was_master/attributes/default.rb
+++ cookbooks/was_master/attributes/default.rb
@@ -7,6 +7,7 @@ default['was']['server_name']   = 'rory'
 default['was']['was_user']	= 'wasadmin'
 default['was']['was_pass']	 = 'adminwas'
 default['was']['soap_port']     = '10003'
+default['was']['host']          = 'STLSCVMG95219'
 
 default['was']['profile_path']  = File.join( node['was']['install_home'], 'profiles', node['was']['profile_name'] )
 
