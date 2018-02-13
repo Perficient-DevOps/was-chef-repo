35c8cf20c74644bd83b9cc5d3bf651cce31b1e51
Sean G Wilbur <sgwilbur@gmail.com>
2018-02-13 12:39:23 -0600

    Updating installer test

--- cookbooks/test_websphere/test/smoke/default/install_test.rb
+++ cookbooks/test_websphere/test/smoke/default/install_test.rb
@@ -4,25 +4,3 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-# Check Dmgr Profile
-describe file( '/opt/IBM/WebSphere/AppServer/profiles/Dmgr001' ) do
- it { should be_directory }
- it { should be_owned_by 'wasadmin' }
-end
-
-describe port(9043) do
-  it { should be_listening }
-  its('processes') {should include 'java'}
-end
-
-describe port(9060) do
-  it { should be_listening }
-  its('processes') {should include 'java'}
-end
-
-# Check Dmgr Profile
-describe file( '/opt/IBM/WebSphere/AppServer/profiles/AppSrv01' ) do
- it { should be_directory }
- it { should be_owned_by 'wasadmin' }
-end
