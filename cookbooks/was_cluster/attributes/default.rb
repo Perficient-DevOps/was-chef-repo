b9c449bfd7269359454529c57909ca1c5821513c
Rory Siems <roryrs@gmail.com>
2018-02-22 12:32:49 -0800

    adding attributes for wrapper cookbook

---
+++ cookbooks/was_cluster/attributes/default.rb
@@ -0,0 +1,6 @@
+#cluster names for specific cluster operations
+default['was']['create_cluster_name'] = 'TestCluster2'
+default['was']['cluster_first_node_name'] = 'STLSCVMG95219Node01'
+default['was']['cluster_first_member_name'] = 'ClusterServerEin'
+default['was']['cluster_subsequent_node_name'] = 'STLSCVMG95218Node02'
+default['was']['cluster_subsequent_member_name'] = 'ClusterServerZwei'
--- cookbooks/was_master/attributes/default.rb
+++ cookbooks/was_master/attributes/default.rb
@@ -28,7 +28,7 @@ default['was']['cluster'] = 'DevCluster'
 default['was']['create_cluster_name'] = 'TestCluster'
 default['was']['cluster_first_node_name'] = 'STLSCVMG95219Node01'
 default['was']['cluster_first_member_name'] = 'roryClusterServer'
-default['was']['cluster_subsequent_node_name'] = 'STLSCVMG95219Node02'
+default['was']['cluster_subsequent_node_name'] = 'STLSCVMG95218Node02'
 default['was']['cluster_subsequent_member_name'] = 'roryClusterServer2'
 
 # App Server
