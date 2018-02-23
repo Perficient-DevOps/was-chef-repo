d11f90804351cc62d58ac3d8e183f98b4a4c7859
LudieGunther <36454743+LudieGunther@users.noreply.github.com>
2018-02-23 11:14:40 -0600

    added comments

--- cookbooks/was_master/files/crtCluster.py
+++ cookbooks/was_master/files/crtCluster.py
@@ -1,26 +1,28 @@
+
+
 clusterName = sys.argv[0]
 debug = sys.argv[1]
 preferLocal="true"
 createReplicationDomain="true"
-
+	
 # first check and see if the cluster already exists
 if AdminConfig.getid("/ServerCluster:" + clusterName + "/") != "":
 	print "Cluster ", clusterName, " already exists."
 	sys.exit(1)
-
+		
 print "Creating cluster"
-
+	
 #Build options list for -clusterConfig step.
 clusterConfigOptions = [clusterName, preferLocal]
-
+	
 #Build options list for -replicationDomain step.
-replicationDomainOptions = []
+replicationDomainOptions = []	
 
 
 if debug == "YES":
 		print "ready to create the cluster "
 	#endIF
-
+	
 try:
 	if debug == "YES":
 		print "before AdminTask.createCluster call."
@@ -31,8 +33,11 @@ try:
 except:
 	print "Exception with AdminTask.createCluster call."
 	sys.exit(1)
-#endExcept
+#endExcept	
 
+if debug == "YES":
+	print "Cluster created, now going to save the configuration."
+#EndIf
 # Save the configuration change.
 #
 if debug == "YES":
@@ -48,8 +53,9 @@ try:
 except:
 	print "Exception with AdminConfig.save call."
 	sys.exit(1)
-#endExcept
-
+#endExcept	
+	
 if debug == "YES":
-		print "Done with cluster create."
+		print "Done with cluster create and save."
 	#endIF
+sys.exit(0)
