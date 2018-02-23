a52a560a220b18df1924913d3b5e11853c951242
LudieGunther <36454743+LudieGunther@users.noreply.github.com>
2018-02-23 14:34:54 -0600

    added runtime comments

--- cookbooks/was_master/files/stopCluster.py
+++ cookbooks/was_master/files/stopCluster.py
@@ -1,48 +1,37 @@
+
+
 clusterName = sys.argv[0]
 debug = sys.argv[1]
 
-
+if debug == "YES":
+	print "First check and see if we have a valid cluster name."
+#EndIf	
 # first check and see if the cluster already exists
 if AdminConfig.getid("/ServerCluster:" + clusterName + "/") == "":
-	print "Cluster ", clusterName, " does not exist."
+	print "Cluster ", clusterName, " does not exist, please specify a valid cluster name."
 	sys.exit(1)
-
-print "Stopping cluster"
-
+			
 
 if debug == "YES":
-		print "ready to stop the cluster "
+		print "Ready to stop the cluster."
 	#endIF
-
+	
 try:
 	if debug == "YES":
-		print "before AdminClusterManagement.stopSingleCluster call."
+		print "Before AdminClusterManagement.stopSingleCluster call."
 	#endIF
 	# Call AdminClusterManagement.stopSingleCluster to start the cluster.
 	AdminClusterManagement.stopSingleCluster(clusterName)
+	if debug == "YES":
+		print "After AdminClusterManagement.stopSingleCluster call - successful."
+	#endIF
 #endTry
 except:
 	print "Exception with AdminClusterManagement.stopSingleCluster call."
 	sys.exit(1)
-#endExcept
+#endExcept	
 
-# Save the configuration change.
-#
-if debug == "YES":
-		print "Stopped the cluster and ready to save."
-	#endIF
-try:
-	if debug == "YES":
-		print "before AdminConfig.save."
-	#endIF
-	# Call save
-	AdminConfig.save()
-#endTry
-except:
-	print "Exception with AdminConfig.save call."
-	sys.exit(1)
-#endExcept
-
+	
 if debug == "YES":
 		print "Done with cluster stop."
 	#endIF
