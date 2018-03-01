1077cddb56ecf8a7dba08121802d85a0a06365c9
Rory Siems <roryrs@gmail.com>
2018-03-01 13:49:36 -0800

    Uploading a hello world cookbook to test chef-client install

--- cookbooks/was_master/recipes/does_cluster_exist.rb
+++ cookbooks/was_master/recipes/does_cluster_exist.rb
@@ -4,28 +4,7 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-jython_script_name = 'crtCluster.py'
 
-directory "#{node['was']['jython_path']}" do
-  owner node[:was][:run_user]
-  group node[:was][:run_group]
-  mode '0755'
-  action :create
-  recursive true
-end
-
-cookbook_file "#{node['was']['jython_path']}/#{jython_script_name}" do
-  source jython_script_name
-  owner node[:was][:run_user]
-  group node[:was][:run_group]
-  mode '0755'
-  action :create
-end
-
-execute "Create Cluster" do
-  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password #{node['was']['was_pass']} -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['create_cluster_name']}\" \"YES\""
-
-not_if{
 jython_script_name = 'existsCluster.py'
 
 directory "#{node['was']['jython_path']}" do
@@ -46,6 +25,4 @@ end
 
 execute "Does Cluster Exist" do
   command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password #{node['was']['was_pass']} -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['create_cluster_name']}\" \"YES\""
-end
-}
 end
