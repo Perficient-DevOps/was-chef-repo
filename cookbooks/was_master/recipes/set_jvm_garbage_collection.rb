73fc5b84ad0aaeb84613a79f115c3f34dffced89
Rory Siems <roryrs@gmail.com>
2018-03-20 14:38:44 -0500

    Adding quotes for javaoption arguments.

--- cookbooks/was_master/recipes/set_jvm_garbage_collection.rb
+++ cookbooks/was_master/recipes/set_jvm_garbage_collection.rb
@@ -22,5 +22,5 @@ cookbook_file "#{node['was']['jython_path']}/#{jython_script_name}" do
 end
 
 execute "Set JVM Garbage Collection" do
-  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password '#{node['was']['was_pass']}' -javaoption #{node['was']['java_option_first']} -javaoption #{node['was']['java_option_second']} -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['node_name']}\" \"#{node['was']['server_name']}\" \"YES\" JVM_ARGS=\"#{node['was']['jvm_gc']}\" JVM_RM_ARGS=\"#{node['was']['jvm_rm_gc']}\""
+  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password '#{node['was']['was_pass']}' -javaoption \"#{node['was']['java_option_first']}\" -javaoption \"#{node['was']['java_option_second']}\" -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['node_name']}\" \"#{node['was']['server_name']}\" \"YES\" JVM_ARGS=\"#{node['was']['jvm_gc']}\" JVM_RM_ARGS=\"#{node['was']['jvm_rm_gc']}\""
 end
