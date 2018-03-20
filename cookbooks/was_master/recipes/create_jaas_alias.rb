296b734e067dbfe0df4ad0bf6b8e804670ea755a
Rory Siems <roryrs@gmail.com>
2018-03-20 12:17:33 -0500

    Update to -javaoption arguments

--- cookbooks/was_master/recipes/create_jaas_alias.rb
+++ cookbooks/was_master/recipes/create_jaas_alias.rb
@@ -22,7 +22,7 @@ cookbook_file "#{node['was']['jython_path']}/#{jython_script_name}" do
 end
 
 execute "Create JAAS Alias" do
-  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password '#{node['was']['was_pass']}' -javaoption #{node['was']['java_option_first'] -javaoption #{node['was']['java_option_second'] -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['jaas_alias_name']}\" \"#{node['was']['jaas_object_username']}\" \"#{node['was']['jaas_object_password']}\" \"#{node['was']['jaas_description']}\" \"YES\""
+  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password '#{node['was']['was_pass']}' -javaoption #{node['was']['java_option_first']} -javaoption #{node['was']['java_option_second']} -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['jaas_alias_name']}\" \"#{node['was']['jaas_object_username']}\" \"#{node['was']['jaas_object_password']}\" \"#{node['was']['jaas_description']}\" \"YES\""
 end
 
 #"myName" "userName" "mypassword" "my description"
