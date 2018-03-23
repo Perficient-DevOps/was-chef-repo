55b6bb573cf1cb5e73d0e9ec8fb4e66bbb428b0d
Rory Siems <roryrs@gmail.com>
2018-03-23 15:52:30 -0700

    Fixing bad character

--- cookbooks/was_master/recipes/create_web_server.rb
+++ cookbooks/was_master/recipes/create_web_server.rb
@@ -22,8 +22,7 @@ cookbook_file "#{node['was']['jython_path']}/#{jython_script_name}" do
 end
 
 execute "Create Web Server" do
-  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password '#{node['was']['was_pass']}' -javaoption \"#{node['was']['java_option_first']}\" -javaoption \"#{node['was']['java_option_second']}\" -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['node_name']}\" \"#{node['was']['web_server_name']}\"
-  \"#{node['was']['web_server_port']}\" \"#{node['was']['plugin_install_root']}\" \"#{node['was']['web_install_root']}\" \"#{node['was']['admin_user_id']}\" \"#{node['was']['admin_password']}\" \"#{node['was']['web_server_type']}\" \"YES\""
+  command "#{node['was']['install_home']}bin/wsadmin.sh -conntype SOAP -host #{ node['was']['host'] } -port #{ node['was']['soap_port'] } -lang jython -user #{node['was']['was_user']} -password '#{node['was']['was_pass']}' -javaoption \"#{node['was']['java_option_first']}\" -javaoption \"#{node['was']['java_option_second']}\" -f #{node['was']['jython_path']}/#{jython_script_name} \"#{node['was']['node_name']}\" \"#{node['was']['web_server_name']}\" \"#{node['was']['web_server_port']}\" \"#{node['was']['plugin_install_root']}\" \"#{node['was']['web_install_root']}\" \"#{node['was']['admin_user_id']}\" \"#{node['was']['admin_password']}\" \"#{node['was']['web_server_type']}\" \"YES\""
 end
 
 #Node name, web server name, web server port, pluginInstallRoot, webInstallRoot, adminUserID, adminPasswd, web server type ( SUN or IHS ), debug
