273cd06e8bfd4f6457c3bd5f1ef640225d8fa1c0
Sean G Wilbur <sgwilbur@gmail.com>
2018-03-30 13:37:16 -0500

    Converted first two recipes to use underlying wasadmin custom resource.

--- cookbooks/was_master/recipes/set_jvm_log_size.rb
+++ cookbooks/was_master/recipes/set_jvm_log_size.rb
@@ -4,7 +4,9 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+# depends on install_jython_scripts
+
 wsadmin 'Set JVM Log Size' do
-  script_name 'setJVMLogSize.py'
+  script_path File.join( node[:was][:jython_path], 'setJVMLogSize.py' )
   script_options "\"#{node['was']['node_name']}\" \"#{node['was']['server_name']}\" \"#{node['was']['log_name']}\" \"#{node['was']['log_size']}\" \"#{node['was']['max_logs_to_keep']}\" \"YES\""
 end
