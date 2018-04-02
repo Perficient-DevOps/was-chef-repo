e25eb00695e6e4ea5c422754898de10e68d32580
Rory Siems <roryrs@gmail.com>
2018-04-02 12:18:13 -0700

    Revert "Updated version in metadata.rb"
    This reverts commit 06cabd8390bc0adafd473ded94f78a562ce61ba8.

--- cookbooks/was_master/metadata.rb
+++ cookbooks/was_master/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@perficient.com'
 license 'All Rights Reserved' # ~FC078
 description 'Installs/Configures was_master'
 long_description 'Installs/Configures was_master'
-version '0.3.2'
+version '0.3.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
