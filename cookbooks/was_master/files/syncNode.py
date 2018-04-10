50a241203ba23a040d349ca38f5647a356f8e4d4
LudieGunther <36454743+LudieGunther@users.noreply.github.com>
2018-04-10 17:56:09 -0500

    Sync single node with active check
    Sync single node with active check

--- cookbooks/was_master/files/syncNode.py
+++ cookbooks/was_master/files/syncNode.py
@@ -1,23 +1,23 @@
 #*************************************************************************
 # Script: syncNode.py - Perform sychronization on a node
 #
-# Description:   Script to sync a given node.
+# Description:   Script to sync a given node. 
 #
 # Parameters:
 #		Script takes two parameters.
 #		1) Node name
-#		5) Debug "YES" or "NO" for printing debug statements for script
+#		5) Debug "YES" or "NO" for printing debug statements for script 
 #
 # Return Codes:
 #		0 = Completed without errors
 #		1 = Error with one of the parameters
-#			*NOTE:  Error processing will validate web application name,
-#				node name and server name for valid objects.   If
-#				there is an error, a corresponding error message will be printed
+#			*NOTE:  Error processing will validate web application name, 
+#				node name and server name for valid objects.   If 
+#				there is an error, a corresponding error message will be printed 
 #				indicating the incorrect parameter.
 #
 # Invocation Example:
-#		./wsadmin.sh -conntype SOAP -host STLSCVMG95219 -port 10003 -lang jython -user wasadmin -password adminwas
+#		./wsadmin.sh -conntype SOAP -host STLSCVMG95219 -port 10003 -lang jython -user wasadmin -password adminwas 
 #                \ -f /WorkingData/jythonScripts/syncNodes.py "STLSCVMG95219Node01" "YES"
 #
 # Author:
@@ -36,52 +36,68 @@ if __name__ == '__main__':
     if len(sys.argv) < 1:
     	print 'need node name, and debug'
         sys.exit(1)
-
+        
 debug=sys.argv[1]
-print "debug value is = ",sys.argv[1]
-print "before check for debug"
+#print "debug value is = ",sys.argv[1]
+#print "before check for debug"
 if debug == "YES":
 	print "args is :"
 	for arg in sys.argv:
     		print arg
-#endIF
-
-# we need to check and see if node and server exists first
-# Check if server exists
-print "before check if node is ok"
-node_id = AdminConfig.getid("/Node:" +sys.argv[0]+"/")
-if (len(node_id) == 0):
-	# server does not exist
-        if debug == "YES":
-        	print "Node = ", sys.argv[0], " was not found."
-        sys.exit(1)
-#endIf
-print "after check is node is OK"
+#endIF 
+nodeName = sys.argv[0]
 if debug == "YES":
-	print "After the node_id and "
-	print node_id
+	print "node name is =",nodeName
 #endIf
 
-# Find JVM
-if debug == "YES":
-	print "before the synch node for node, ", node_id
-#endIf
+#check to see if the node exists
+nodeRtn = AdminNodeManagement.doesNodeExist(nodeName)
+print "return value from does node exist is =",nodeRtn
+if nodeRtn == "true":
+	# we have a valid node name, next check and see if it is running
+	nodeActive = AdminNodeManagement.isNodeRunning(nodeName)
+	print "return value from is active is =",nodeActive
+	if nodeActive == 1: 
+		print " we have a valid and active node that can be sychronized."
+		# Find JVM
+		
 
-Sync1 = AdminControl.completeObjectName('type=NodeSync,node='+sys.argv[0]+',*')
-if debug == "YES":
-	print "after complete objectname and object is =",Sync1
-#endIf
-rtn = AdminControl.invoke(Sync1, 'sync')
-if debug == "YES":
-	print "After sync and returnvalue is =",rtn
-#endIf
-
-# now lets test returncode from sync to be sure it worked
-if rtn == "true":
-	# we have a valid sychronization of the node
-	sys.exit(0)
+		Sync1 = AdminControl.completeObjectName('type=NodeSync,node='+nodeName+',*')
+		if debug == "YES":
+			print "after complete objectname and object is =",Sync1
+		#endIf
+		rtn = AdminControl.invoke(Sync1, 'sync')
+		if debug == "YES":
+			print "After sync and returnvalue is =",rtn
+		#endIf
+		# now lets test returncode from sync to be sure it worked
+		if rtn == "true":
+			# we have a valid sychronization of the node
+			if debug == "YES":
+				print "We have a valid sychronization!"
+			#endIf
+			sys.exit(0)
+		else:
+			# we do not have a valid sync.
+			if debug == "YES":
+				print "We have an invalid sychronization!"
+			#endIf
+			sys.exit(1)
+	#EndIf
+	else:
+		print "We have a valid node agent but it is not active.  No Sync. "
+		sys.exit(0)
+	#EndElse
+#EndIf
 else:
-	# we do not have a valid sync.
+	if debug == "YES":
+		print "Invalid node name.  Please specify a correct node name."
+	#EndIf
 	sys.exit(1)
+#EndElse
+	
+if debug == "YES":
+	print "Done with sync node script."
+#EndIf
 
-sys.exit(1)
+
